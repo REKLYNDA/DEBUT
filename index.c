a1a3ead08961fb54c6920d60d5be429b1831cf8b
REKLYNDA <relykaa@gmail.com>
2018-09-06 23:33:31 +0200

    première sauvegarde

---
+++ index.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void addition(void)
+{
+    printf("1+1 = %d",2);
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "index.h"
+
+int main()
+{
+    addition();
+    return EXIT_SUCCESS;
+}
